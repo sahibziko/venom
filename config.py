50f506f052e99d64d0fc39f4aeba6118d530c9ed
Ferid Abusov <122831032+Theferid@users.noreply.github.com>
2024-02-04 03:55:46 +0400

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://t.me/The_ferid",
+    "https://t.me/FerooBotlar",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
