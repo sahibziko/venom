d8cd715c4db12a099c39efed9a02b9cf736b105c
Abbasov04 <148398762+Abbasov04@users.noreply.github.com>
2024-03-13 04:05:26 +0400

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Abbasov04/Abbasov04",
+    "https://github.com/Abbasov04/YASEMEN.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
